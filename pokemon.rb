7b02597e4991cd269fef49dfcf2f03b0ece49bfc
Jeffrey Barszcz <jfbarszcz@gmail.com>
2015-03-18 17:46:23 -0400

    Basic Pokemon setup and attacking, types and overkill not yet handled

--- pokemon.rb
+++ pokemon.rb
@@ -1,1 +1,30 @@
-# A placeholder for some exciting Pokemon code!
+class Pokemon
+
+  attr_reader :hp
+
+  MAX_HP = 100 # let's not hardcode this in #initialize
+
+  def initialize
+    @hp = MAX_HP
+  end
+
+  def attack(other_poke)
+    other_poke.take_damage
+  end
+  
+  protected
+
+  def take_damage
+    @hp -= 10
+  end
+  
+end
+
+class Squirtle < Pokemon  
+end
+
+class Charmander < Pokemon  
+end
+
+class Bulbasaur < Pokemon  
+end
